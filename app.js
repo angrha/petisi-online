38314eef19aac7e29b4325926a43c054e9a85e83
tomybudiman <tommybudi1998@gmail.com>
2017-11-07 15:28:17 +0700

    Ku serahkan pada Aang

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ app.set("views","./public");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
-const index=require("./router/index.js");
+const index=require("./router/index");
 app.use("/",index);
+
+const register=require("./router/register");
+app.use("/register",register);
 
 app.listen(3000,()=>{
     console.log("Listen to me now! I'm working on port 3000!");
